bf12f6afcb995ab85d5fea4555a90490111ed6fe
Margherita88 <lilac.spring2403@gmail.com>
2025-09-09 13:42:37 +0900

    Day12_為替レート表示（外部API）ExchangeRate-APIでAPI取得してjsonで確認

---
+++ Day12_currency_app/app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify, jsonify, render_template
+from flask.cli import load_dotenv
+import requests
+
+load_dotenv() # .env を読み込む
+API_KEY = os.getenv('API_KEY') # .envを使うために必要
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD'
+    response = requests.get(url)
+    data = response.json()
+    return  jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
