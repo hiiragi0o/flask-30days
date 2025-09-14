ec2c32d8ac1f093a954242a46ca1c41f4d669107
Margherita88 <lilac.spring2403@gmail.com>
2025-09-14 19:57:58 +0900

    Day16_TODOリスト（SQLite保存） - 新しい ToDo を追加、削除、更新するためのルートを追加

--- Day16_todo_sqlite_app/version1/app.py
+++ Day16_todo_sqlite_app/version1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -20,13 +20,34 @@ def index():
     # 全ての Todo を表示
     todo_list = Todo.query.all()
     print(todo_list)
-    return render_template('base.html')
+    return render_template('base.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    # アイテムを追加する
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    # アイテムを更新する
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    # アイテムを削除する
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     with app.app_context():  # ← アプリケーションコンテキストを確保
         db.create_all()
-
-        new_todo = Todo(title='todo 1', complete=False)
-        db.session.add(new_todo)
-        db.session.commit()
     app.run(debug=True)
