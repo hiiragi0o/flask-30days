38a9d72eba8655906a15e26cf3ba162d5b44abcb
Margherita88 <lilac.spring2403@gmail.com>
2025-09-15 21:05:08 +0900

    Day13_ニュース一覧（NewsAPI）-キーワード検索機能を実装。フォームと検索結果を表示。jinjaタグフォーマットのため.prettierrc を追加

--- Day13_news_app/version2/app.py
+++ Day13_news_app/version2/app.py
@@ -1,7 +1,6 @@
-# HTML テンプレートを使って画面表示
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from newsapi import NewsApiClient # requests の代わりに newsapi 公式ライブラリを使う
 
 load_dotenv()
@@ -10,12 +9,19 @@ API_KEY = os.getenv('API_KEY')
 app = Flask(__name__)
 newsapi = NewsApiClient(api_key=API_KEY) # NewsApiClient のインスタンスを作成
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # アメリカのニュース top-headlines を取得
-    top_headlines =  newsapi.get_top_headlines(language='en',country='us')
-    articles = top_headlines['articles'] # 記事部分だけ取り出す
-    return render_template('index.html', articles=articles)
+    if request.method == 'POST':
+        keyword = request.form['keyword']
+        news = newsapi.get_everything(q=keyword, language='en', sort_by='relevancy') # 関連度順にソート
+        articles = news['articles'] # 記事部分だけ取り出す
+        return render_template('index.html', articles=articles, keyword=keyword)
+
+    else:
+        # keyword がない時 top-headlines を取得
+        top_headlines =  newsapi.get_top_headlines(language='en',country='us')
+        articles = top_headlines['articles'] # 記事部分だけ取り出す
+        return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
