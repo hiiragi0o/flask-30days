5463400f5a0116201105ca377530a4abbc5b2952
Margherita88 <lilac.spring2403@gmail.com>
2025-09-17 22:13:19 +0900

    Day17_掲示板（SQLite）- 画像のパス保存用カラムを追加。マイグレーション機能を追加。

--- Day17_bbs_sqlite_app/app.py
+++ Day17_bbs_sqlite_app/app.py
@@ -1,6 +1,11 @@
 from datetime import datetime
+import os
+
 from flask import Flask, redirect, render_template, request
+from flask_migrate import Migrate
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import pytz
 
 app = Flask(__name__)
@@ -15,9 +20,17 @@ DB_INFO = {
 }
 # データベースの接続情報の文字列を作成
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg://{user}:{password}@{host}/{name}'.format(**DB_INFO)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SECRET_KEY"] = os.urandom(24)
+
 # SQL Alchemyのインスタンスのメソッド init_app に flaskインスタンス を渡して実行
 db.init_app(app)
+
+migrate = Migrate(app, db) # Migrateのインスタンスを作成
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # Post用のモデルクラス
 class Post(db.Model):
@@ -26,6 +39,7 @@ class Post(db.Model):
     body =db.Column(db.String(300), nullable=False)
     tokyo_timezone = pytz.timezone('Asia/Tokyo')
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(tokyo_timezone))
+    img_name = db.Column(db.String(300), nullable=True)
 
 @app.route('/')
 def index():
