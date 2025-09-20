96f8ada85dfd0cd1a13de4845941a26987bc00ee
Margherita88 <lilac.spring2403@gmail.com>
2025-09-20 19:13:14 +0900

    Day20_投票アプリ - 投票選択肢を管理するテーブルを作成

--- Day20_voting_app/app.py
+++ Day20_voting_app/app.py
@@ -1,8 +1,35 @@
-# Flask アプリ基本構造
+# 投票選択肢を管理する choices テーブルを作成
 from flask import Flask, redirect, render_template, request, url_for
 import sqlite3
 
 app = Flask(__name__)
+
+# app.py の init_db() を更新
+def init_db():
+    conn = sqlite3.connect('votes.db')
+    c = conn.cursor()
+
+    # 投票結果テーブル(votes) 更新
+    # votes に choice_id を外部キーとして保持
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS votes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            choice_id INTEGER NOT NULL,
+            FOREIGN KEY(choice_id) REFERENCES choices(id)
+        )
+        ''')
+
+    # 選択肢テーブル(choices)
+    # choices テーブルに管理者が追加した選択肢を保存
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS choices (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    print("choicesテーブルを作成しました")
 
 @app.route("/")
 def index():
@@ -29,4 +56,5 @@ def result():
     return render_template('result.html', results=results)
 
 if __name__ == '__main__':
+    init_db() # テーブル作成
     app.run(debug=True)
