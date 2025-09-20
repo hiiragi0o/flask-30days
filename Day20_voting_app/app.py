7c4f54b476042becd56490cb948588eae9fcb83e
Margherita88 <lilac.spring2403@gmail.com>
2025-09-20 18:10:29 +0900

    Day20_投票アプリ - 投票機能の実装と結果表示機能の追加

--- Day20_voting_app/app.py
+++ Day20_voting_app/app.py
@@ -1,18 +1,32 @@
-# SQLite データベース作成
+# Flask アプリ基本構造
+from flask import Flask, redirect, render_template, request, url_for
 import sqlite3
 
-def init_db():
-    conn = sqlite3.connect('vote.db')
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/vote', methods=['POST'])
+def vote():
+    choice = request.form.get('choice')
+    conn = sqlite3.connect('votes.db')
     c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS votes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            choice TEXT NOT NULL
-        )
-        ''')
-    
+    c.execute('INSERT INTO votes (choice) VALUES (?)', (choice,))
     conn.commit()
     conn.close()
+    return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    conn = sqlite3.connect('votes.db')
+    c = conn.cursor()
+    # choice を取得、その行数（COUNT(*)）を取得、同じchoiceをグループ化
+    c.execute('SELECT choice, COUNT(*) FROM votes GROUP BY choice')
+    results = c.fetchall()  # [("A", 10), ("B", 5)]
+    conn.close()
+    return render_template('result.html', results=results)
 
 if __name__ == '__main__':
-    init_db()
+    app.run(debug=True)
