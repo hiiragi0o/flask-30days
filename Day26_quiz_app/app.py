200f75e10772f5f2ec1837f234302f2d3326b777
Margherita88 <lilac.spring2403@gmail.com>
2025-09-26 23:19:56 +0900

    Day26_クイズアプリ - 複数のクイズ機能の実装

--- Day26_quiz_app/app.py
+++ Day26_quiz_app/app.py
@@ -1,26 +1,67 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 # 問題
-quiz = {
+quizzes = [
+    {
     'question': 'Pythonのファイル拡張子は？',
     'choices': ['py', 'js', 'html', 'css'],
     'answer': 'py'
+    },
+    {
+    'question': 'HTMLの見出しタグは？',
+    'choices': ['<p>', '<h1>', '<div>', '<title>'],
+    'answer': '<h1>'
+    },
+    {
+    'question': 'Flaskは何のフレームワーク？',
+    'choices': ['Web', 'AI', 'ゲーム', '画像処理'],
+    'answer': 'Web'
     }
+]
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    # 最初の問題（0番目）にリダイレクト
+    return redirect(url_for('quiz', question_index=0))
+
+@app.route('/<int:question_index>', methods=['GET', 'POST'])
+def quiz(question_index): # 引数
+    if question_index >= len(quizzes):
+        return 'クイズ終了！お疲れさまでした。'
+    
+    quiz = quizzes[question_index]
     result = None
+    answerd = False
+
     if request.method == 'POST':
         selected = request.form.get('choice')
         if selected == quiz['answer']:
-            return f"正解です！"
+            result =  f"正解です！"
         else:
             # f-stringを使用して変数の値を埋め込む
-            return f"不正解です。正しい回答は {quiz['answer']} です"
+            result =  f"不正解です。正しい回答は {quiz['answer']} です"
+        
+        answerd = True # 回答済みにする
 
-    return render_template('index.html', quiz=quiz, result=result)
+        return render_template(
+            'quiz.html',
+            quiz=quiz,
+            question_index=question_index,
+            result=result,
+            answerd=answerd,
+            total=len(quizzes)
+        )
+
+    # GET時
+    return render_template(
+        'quiz.html', 
+        quiz=quiz, 
+        question_index=question_index,
+        answerd=answerd,
+        total=len(quizzes)
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
