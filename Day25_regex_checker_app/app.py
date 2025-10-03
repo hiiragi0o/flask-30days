2dd80b774847db887f9fdfb71d9b351efc463e57
Margherita88 <lilac.spring2403@gmail.com>
2025-10-03 23:03:21 +0900

    Day25_正規表現チェッカー - 修正

--- Day25_regex_checker_app/app.py
+++ Day25_regex_checker_app/app.py
@@ -6,16 +6,36 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
+    
+    # options を定義
+    options = {'ignorecase':False, 'multiline': False, 'dotall':False}
+
     if request.method == 'POST':
         pattern = request.form.get('pattern')
         text = request.form.get('text')
 
+        # 正規表現のオプションを取得
+        options['ignorecase'] = bool (request.form.get('ignorecase'))
+        options['multiline'] = bool(request.form.get('multiline'))
+        options['dotall'] = bool(request.form.get('dotall'))
+        
+
+        # 正規表現のオプションを設定
+        flags = 0
+        if options['ignorecase']:
+            flags |= re.IGNORECASE
+        if options['multiline']:
+            flags |= re.MULTILINE
+        if options['dotall']:
+            flags |= re.DOTALL
+
+        # 安全にコンパイルして使う
         try:
-            matches = re.findall(pattern, text) # 正規表現でマッチする部分をすべて抽出
+            matches = re.findall(pattern, text, flags=flags) # 正規表現でマッチする部分をすべて抽出
             result = matches if matches else 'マッチなし' # マッチがなければ「マッチなし」と表示
         except re.error as e:
             result = f'正規表現エラー: {e}'
-    return render_template('index.html', result=result)
+    return render_template('index.html', result=result, options=options)
 
 if __name__ == '__main__':
     app.run(debug=True)
