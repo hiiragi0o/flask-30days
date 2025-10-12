80edddeb13a95aba9fffa6304c749cbe6d3d7076
Margherita88 <lilac.spring2403@gmail.com>
2025-10-13 00:28:42 +0900

    Day11_天気情報アプリ - 現在の天気に加えて、5日間の天気を表示する

--- Day11_weather_app/version3/app.py
+++ Day11_weather_app/version3/app.py
@@ -18,31 +18,68 @@ cities = {
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    weather_data = None
+    current_weather = None
+    forecast_list = []
     error = None
 
     if request.method == 'POST':
         city_name = request.form.get('area') # フォームから都市を取得
 
-        # OpenWeatherMap API
-        api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=ja&units=metric'
-        response = requests.get(api_url)
-        data = response.json()
+        # OpenWeatherMap API(5日間/3時間ごとの予報)
+        weather_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=ja&units=metric'
+        forecast_url = f'https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_KEY}&lang=ja&units=metric'
+
+        weather_res = requests.get(weather_url)
+        forecast_res = requests.get(forecast_url)
+        weather_data = weather_res.json()
+        forecast_data = forecast_res.json()
 
         # エラーハンドリング
-        if response.status_code != 200 or 'main' not in data:
-            error = data.get('message', '天気情報を取得できませんでした。')
+        if weather_res.status_code != 200 or forecast_res.status_code != 200:
+            error = weather_data.get('message', '天気情報を取得できませんでした。')
 
         else:
-            # 取得した天気データから必要なデータを抽出
-            weather_data = {
-                'city': data['name'],
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description'],
-                'icon': data['weather'][0]['icon']
+            # 現在の天気から必要なデータを抽出
+            current_weather = {
+                'city': weather_data['name'],
+                'temperature': weather_data['main']['temp'],
+                'description': weather_data['weather'][0]['description'],
+                'icon': weather_data['weather'][0]['icon']
             }
+
+            # 5日間の天気（3時間ごとのデータ → 1日ごとにまとめる）
+            forecasts = {}
+            for item in forecast_data['list']:
+                data_text = item['dt_txt'].split(" ")[0]  # "2025-10-12"
+                if data_text not in forecasts:
+                    forecasts[data_text] = {
+                        'temp':[],
+                        'weather':[],
+                        'icons':[]
+                    }
+                forecasts[data_text]['temp'].append(item['main']['temp'])
+                forecasts[data_text]['weather'].append(item['weather'][0]['description'])
+                forecasts[data_text]['icons'].append(item['weather'][0]['icon'])
+            
+            # 平均気温と代表的な天気を抽出
+            for date, info in forecasts.items():
+                avf_temp = round(sum(info['temp']) / len(info['temp']), 1)
+                desc = max(set(info['weather']), key=info['weather'].count)
+                icon = info['icons'][len(info['icons']) // 2] # 中間のアイコンを代表として使う
+                forecast_list.append({
+                    'date': date,
+                    'temp': avf_temp,
+                    'description': desc,
+                    'icon': icon
+                })
     
-    return render_template('index.html', cities=cities, error=error, weather_data=weather_data)
+    return render_template(
+        'index.html', 
+        cities=cities, 
+        error=error, 
+        current_weather=current_weather,
+        forecast_list=forecast_list
+    )
 
 
 if __name__ == '__main__':
